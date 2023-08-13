40ef637fec3381456bc8337bee63ff994ceb4db5
Agundez, Rodrigo <rodrigo.agundez@dyson.com>
2023-08-13 20:26:32 +0800

    Refactor dependency managment with use of pyproject.toml

--- shell/format.sh
+++ shell/format.sh
@@ -3,7 +3,6 @@
 base_dir=$(dirname $(dirname $0))
 targets="${base_dir}/*.py ${base_dir}/keras_core/ ${base_dir}/benchmarks/"
 
-isort --sp "${base_dir}/pyproject.toml" ${targets}
-black --config "${base_dir}/pyproject.toml" ${targets}
-
-flake8 --config "${base_dir}/setup.cfg" ${targets}
+isort ${targets}
+black ${targets}
+flake8 ${targets}
